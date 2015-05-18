56e8e7d72b069e33eafaf2ab796f6feaebd15765
amani12 <amani.younes2@gmail.com>
2015-05-18 21:46:29 +0200

    modif

---
+++ inc/footer.inc.php
@@ -0,0 +1,5 @@
+<div id="lowerpage-footer">
+<a href="index.php" class="tile">Informations l&eacute;gales</a> |<a href="index.php" class="tile">Contacter Webmaster</a> |<a href="index.php" class="tile">Plan du site</a>
+</div>
+</body>
+</html>
---
+++ inc/header.inc.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
+<head>
+	<meta charset="ISO-8859-1">
+	<title>Gymnase UHA</title>
+	<link rel="stylesheet" href="../../css/accueil.css">
+</head>
+<body>
+<div id ="header">
+  <ul id="portal-siteactions">
+
+    <li id="siteaction-plan_acces"><a href="http://www.uha.fr/planacces" accesskey="" title="Plan d'accès">Plan d'acc&egrave;s</a></li>
+    <li id="siteaction-contact-uha"><a href="http://www.uha.fr/contact-uha" accesskey="" title="Contact">Contact</a></li>
+    <li id="siteaction-accueil"><a href="http://www.uha.fr/" accesskey="" title="Accueil">Accueil</a></li>
+
+</ul>
+ 
+</div>
+<div id="emptyspace"></div>
+<div id ="lowerpage">
+  <table id ="lowerpage-column">
+    <tbody>
+      <tr>
+	     <td id="lowerpage-column-one">
+		    <dl class="portlet" id="portlet-navigation-tree">
+               <dt class="portletHeader">
+                  <span class="portletTopLeft"></span>
+        <a href="index.php" class="tile">Navigation</a>
+       
+        <span class="portletTopRight"></span>
+    </dt>
+
+    <dd class="portletItem lastItem">
+        <ul class="portletNavigationTree navTreeLevel0">
+
+            <li class="navTreeItem">
+                
+                   <div class="visualIcon contenttype-plone-site">
+                       <a class="navTreeCurrentItem visualIconPadding" href="index.php" title="">
+                       Accueil
+                       
+                       </a>
+                   </div>
+                
+            </li>
+            
+
+
+
+<li class="navTreeItem visualNoMarker">
+
+    
+
+    <div class="visualIcon contenttype-folder">
+        
+        <a href="http://www.uha.fr/international-relations" class="state-published visualIconPadding" title="">Contrat pedagogique</a>
+        
+    </div>
+
+    
+    
+</li>
+
+
+<li class="navTreeItem visualNoMarker">
+
+    
+
+    <div class="visualIcon contenttype-folder">
+        
+        <a href="http://www.uha.fr/presentation-en" class="state-published visualIconPadding" title="">Projet Pedagogique</a>
+        
+    </div>
+
+    
+    
+</li>
+
+
+<li class="navTreeItem visualNoMarker">
+
+    
+
+    <div class="visualIcon contenttype-folder">
+        
+        <a href="http://www.uha.fr/research" class="state-published visualIconPadding" title="">Emplois du temps</a>
+        
+    </div>
+
+    
+    
+</li>
+
+<li class="navTreeItem visualNoMarker">
+
+    
+
+    <div class="visualIcon contenttype-folder">
+        
+        <a href="http://www.uha.fr/research" class="state-published visualIconPadding" title="">Guide des Activites</a>
+        
+    </div>
+
+    
+    
+</li>
+
+
+
+
+        </ul>
+        <span class="portletBottomLeft"></span>
+        <span class="portletBottomRight"></span>
+    </dd>
+</dl>
+</td>
