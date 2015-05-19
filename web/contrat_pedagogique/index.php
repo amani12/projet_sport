5902129b4c19d25b1a0b1c0a10ae94bf6588ea72
amani12 <amani.younes2@gmail.com>
2015-05-19 22:13:47 +0200

    contrat pedagogique

---
+++ web/contrat_pedagogique/index.php
@@ -0,0 +1,38 @@
+<?php
+require_once('../../inc/header.inc.php');
+?>
+
+		 
+		 
+		 <td  id="lowerpage-column-content">
+			Contrat Pédagogique pour l'option LMD "Sport"
+			
+			L'UE  « pratique des activités physiques, sportives et de plein air » ou « sport » fait partie des UE libres de l'université de Haute-Alsace.
+			Vous pouvez valider 3 crédits par an pendant votre Licence et votre Master.
+			
+			Votre contrat prévoit 50 heures de sport, pratiquées régulièrement tout au long de l’année, soit 2 heures de sport par semaine. L’une ou l’autre absence justifiée sera tolérée (examen, stage, maladie ou autre). Dans certains cas, (départ en stage au 2 e semestre, retard à l’inscription…) vous avez la possibilité de pratiquer 50h sur un semestre, soit 4 heures de sport par semaine.
+			Pour plus de détail (voir guide des APS)
+			Barème :
+			<li>progression personnelle dans l’activité : 10 points</li>
+			<li>Progression : 6 points<li>
+			<li>L’évaluation a lieu au regard de sa progression personnelle dans l’activité, que l’étudiant se situe au niveau  de la découverte, de l’initiation ou du perfectionnement.
+Pour les experts, l’évaluation pourra avoir lieu au regard de leurs investissements et de leurs prises  de responsabilités dans le sport universitaire : animation des entrainements FFSU, suivi en compétition par exemple. Dans ce cas, un compte-rendu d’activité d’au moins deux pages sera imposé.
+<li>
+			<li>Présence : 4 points<li>
+			<li>Niveau d’habiletés en cours : 4 points<li>
+			<li>Activités complémentaires de l’étudiant : 6 points.<li>
+			<li>Participation a des activités ponctuelles (duathlon, nuit de badminton, soirées danse, rencontres amicales Mulhouse/Colmar, plein-air …)<li>
+			<li>Représentation de l’UHA en compétition universitaire.</li>
+			<li>Participation régulière à des compétitions civiles (a condition d’avoir obtenu au moins 12/20 dans la note SUAPS – attestation officielle exigée).</li>
+			<li>Ponctualité, tenue, mise en place/rangement du matériel, faculté d’adaptation au groupe, capacité à prendre des initiatives, connaissance des règlements sportifs.</li>
+
+			
+		 </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+<?php
+require_once('../../inc/footer.inc.php');
+?>
