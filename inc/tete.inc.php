d8b7fcb1191791aba546db7ee3bc087402ee39d9
amani12 <amani.younes2@gmail.com>
2015-05-20 14:11:01 +0200

    tete

---
+++ inc/tete.inc.php
@@ -0,0 +1,108 @@
+<div id ="header">
+  <ul id="portal-siteactions">
+
+    <li id="siteaction-plan_acces"><a href="http://www.uha.fr/planacces" accesskey="" title="Plan d'accès">Plan d'acc&egrave;s</a></li>
+    <li id="siteaction-contact-uha"><a href="http://www.uha.fr/contact-uha" accesskey="" title="Contact">Contact</a></li>
+    <li id="siteaction-accueil"><a href="http://www.uha.fr/" accesskey="" title="Accueil">Accueil</a></li>
+
+</ul>
+ 
+</div>
+<div id="emptyspace"></div>
+<div id ="lowerpage">
+  <table id ="lowerpage-column">
+    <tbody>
+      <tr>
+	     <td id="lowerpage-column-one">
+		    <dl class="portlet" id="portlet-navigation-tree">
+               <dt class="portletHeader">
+                  <span class="portletTopLeft"></span>
+        <a href="index.php" class="tile">Navigation</a>
+       
+        <span class="portletTopRight"></span>
+    </dt>
+
+    <dd class="portletItem lastItem">
+        <ul class="portletNavigationTree navTreeLevel0">
+
+            <li class="navTreeItem">
+                
+                   <div class="visualIcon contenttype-plone-site">
+                       <a class="navTreeCurrentItem visualIconPadding" href="../" title="">
+                       Accueil
+                       
+                       </a>
+                   </div>
+                
+            </li>
+            
+
+
+
+<li class="navTreeItem visualNoMarker">
+
+    
+
+    <div class="visualIcon contenttype-folder">
+        
+        <a href="../contrat_pedagogique/" class="state-published visualIconPadding" title="">Contrat pedagogique</a>
+        
+    </div>
+
+    
+    
+</li>
+
+
+<li class="navTreeItem visualNoMarker">
+
+    
+
+    <div class="visualIcon contenttype-folder">
+        
+        <a href="http://www.uha.fr/presentation-en" class="state-published visualIconPadding" title="">Projet Pedagogique</a>
+        
+    </div>
+
+    
+    
+</li>
+
+
+<li class="navTreeItem visualNoMarker">
+
+    
+
+    <div class="visualIcon contenttype-folder">
+        
+        <a href="http://www.uha.fr/research" class="state-published visualIconPadding" title="">Emplois du temps</a>
+        
+    </div>
+
+    
+    
+</li>
+
+<li class="navTreeItem visualNoMarker">
+
+    
+
+    <div class="visualIcon contenttype-folder">
+        
+        <a href="" class="state-published visualIconPadding" title="">Guide des Activites</a>
+        
+    </div>
+
+    
+    
+</li>
+
+
+
+
+        </ul>
+        <span class="portletBottomLeft"></span>
+        <span class="portletBottomRight"></span>
+    </dd>
+</dl>
+</td>
