5e490a4fb4ee11233d172cfb160c178a1ad7ac96
amani12 <amani.younes2@gmail.com>
2015-05-20 14:46:34 +0200

    header

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -5,6 +5,23 @@
 	<title>Gymnase UHA</title>
 	<link rel="stylesheet" href="../../css/accueil.css">
 </head>
+
+	<script src="../../js/jquery-2.1.1.min.js"></script>
+	<script src="../../js/jquery-ui.js"></script>
+	<script src="../../js/fancybox/source/jquery.fancybox.pack.js"></script>
+	<script>
+	$(function()
+	{
+		// Pour le suivi des commandes (admin)
+		$("#fancy").fancybox({
+			'width':'35%',
+			'height':'50%',
+			'type':'iframe',
+			'autoScale':'false'
+        });
+	});
+	</script>
+</head>
 <body>
 <?php require_once('config.php');
 	  require_once('../../lib/lib_db.class.php');
