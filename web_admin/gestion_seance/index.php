934c375828ac42d2cab9271df8e781eb5f11acc0
fellyralph <feliquestious@yahoo.ca>
2015-05-30 21:53:19 +0200

    gestion of seance
    gestion of seance

--- web_admin/gestion_seance/index.php
+++ web_admin/gestion_seance/index.php
@@ -1,11 +1,91 @@
 <?php
 session_start();
+if(!$_SESSION['isLogged']) {
+  header("location:http://localhost/projet_sport/web_admin/connexion/"); 
+  die(); 
+}
 require_once('../../inc/teteadmin.inc.php');
 ?>
 				<td>
 					<div id="contents">
-					<p style="color:#185dc6;font-size:30px;" align="center">Bienvenue dans la page Accueil de Site des UE libre Sport</p>
-					<img src="../../img/gymnase.jpg" alt="Gymnase Universitaire" style="width:700px;height:200px;">
+						   <div id="choix">
+						   <table>
+						     <tr>
+						           <td>
+								   <b>Activite:</b>
+								   </td>
+								   <td>
+										  <select>
+												   <option value="Badminton">Badminton</option>
+												   <option value="Natation">Natation</option>
+												   <option value="Volley-ball">Volley-ball</option>
+												   <option value="Musculation">Musculation</option>
+											</select>
+								    </td>
+									<td style="width:100px">
+									</td>
+									
+								   <td>
+								   <b>Date:</b>
+								   </td>
+								   <td>
+										   <select>
+												   <option value="datekey1">22/02/2016</option>
+												   <option value="datekey2">28/02/2016</option>
+												   <option value="datekey3">06/03/2016</option>
+												   <option value="datekey3">13/03/2016</option>
+											</select>
+								    </td>			
+							   </tr>				
+						    </table>
+                            </div>	
+                             <div  id="infos_seance">
+							    <table border=1>
+								   <tr>
+								     <th>
+									 Séance
+									 </th>
+									 <th>
+									 Date
+									 </th>
+									 <th>
+									 Modifier
+									 </th>
+									 <th>
+									 Annuler
+									 </th>
+									 <th>
+									 Presence
+									 </th>
+								   </tr>
+								    <tr>
+								     <td>
+									 Séance
+									 </td>
+									 <td>
+									 Date
+									 </td>
+									 <td>
+									 Modifier
+									 </td>
+									 <td>
+									 Annuler
+									 </td>
+									 <td>
+									 Presence
+									 </td>
+								   </tr>
+								</table>
+                             </div>
+							 <div>
+							 
+							    <form action="http://google.com">
+                                     <input type="submit" value="Ajouter une seance">
+                                 </form>
+							 </div>
+                             							 
+						  
+						
 					</div>
 				</td>
 			</tr>
