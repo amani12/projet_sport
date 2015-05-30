80e754c7b1fb38461a508c5721bbcb59f300d99a
fellyralph <feliquestious@yahoo.ca>
2015-05-30 19:59:44 +0200

    changement de nom de doessier
    le dossier inscription est maintenant renommé connexion_admini

---
+++ web_admin/gestion_seance/index.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+require_once('../../inc/teteadmin.inc.php');
+?>
+				<td>
+					<div id="contents">
+					<p style="color:#185dc6;font-size:30px;" align="center">Bienvenue dans la page Accueil de Site des UE libre Sport</p>
+					<img src="../../img/gymnase.jpg" alt="Gymnase Universitaire" style="width:700px;height:200px;">
+					</div>
+				</td>
+			</tr>
+	   </table>
+<?php
+require_once('../../inc/footeradmin.inc.php');
+?>	   
+	   
