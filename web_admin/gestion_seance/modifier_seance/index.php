f6a9de4cb471de5f6e4c6402e8f54a183e7b732c
amani12 <amani.younes2@gmail.com>
2015-06-01 19:12:34 +0200

    c

--- web_admin/gestion_seance/modifier_seance/index.php
+++ web_admin/gestion_seance/modifier_seance/index.php
@@ -1,6 +1,6 @@
 <?php
-require_once('../../inc/header.inc.php');
-require_once('../../inc/teteadmin.inc.php');
+//require_once('../../../inc/header.inc.php');
+require_once('../../../inc/teteadmin.inc.php');
 $db=new DB_connection();
 $req="select * from activite where numero_personne='".$_SESSION['numero']."' and etat=1";
 $db->DB_query($req);
